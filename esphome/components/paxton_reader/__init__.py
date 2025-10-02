4cd6044652d7dcb3bc6dfc127058f9b162b470d5
Jonny Davey <Jonny190@gmail.com>
2025-10-02 12:32:27 +0100

    Updates to init

--- esphome/components/paxton_reader/__init__.py
+++ esphome/components/paxton_reader/__init__.py
@@ -1,50 +1,28 @@
+# esphome/components/paxton_reader/__init__.py
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor, sensor, binary_sensor
 from esphome import pins
+from esphome.const import CONF_ID   # <-- add this import
 
 DEPENDENCIES = ["logger"]
+AUTO_LOAD = []
 
 paxton_ns = cg.esphome_ns.namespace("paxton")
 PaxtonReader = paxton_ns.class_("PaxtonReader", cg.Component)
 
-CONF_CLOCK_PIN = "clock_pin"
-CONF_DATA_PIN = "data_pin"
-CONF_LED_GREEN = "led_green"
-CONF_LED_YELLOW = "led_yellow"
-CONF_LED_RED = "led_red"
-CONF_LAST_CARD = "last_card"
-CONF_CARD_TYPE = "card_type"
-CONF_CARD_COLOUR = "card_colour"
-CONF_BIT_COUNT = "bit_count"
-CONF_READING = "reading"
-CONF_NET2_BITS = "net2_bits"
-CONF_SWITCH2_BITS = "switch2_bits"
-CONF_DEBOUNCE_US = "debounce_us"
+# ... (constants unchanged)
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(PaxtonReader),
-        cv.Required(CONF_CLOCK_PIN): pins.internal_gpio_input_pin_schema,
-        cv.Required(CONF_DATA_PIN): pins.internal_gpio_input_pin_schema,
-        cv.Optional(CONF_LED_GREEN): pins.gpio_output_pin_schema,
-        cv.Optional(CONF_LED_YELLOW): pins.gpio_output_pin_schema,
-        cv.Optional(CONF_LED_RED): pins.gpio_output_pin_schema,
-
-        cv.Required(CONF_LAST_CARD): cv.use_id(text_sensor.TextSensor),
-        cv.Required(CONF_CARD_TYPE): cv.use_id(text_sensor.TextSensor),
-        cv.Required(CONF_CARD_COLOUR): cv.use_id(text_sensor.TextSensor),
-        cv.Required(CONF_BIT_COUNT): cv.use_id(sensor.Sensor),
-        cv.Required(CONF_READING): cv.use_id(binary_sensor.BinarySensor),
-
-        cv.Optional(CONF_NET2_BITS, default=75): cv.positive_int,
-        cv.Optional(CONF_SWITCH2_BITS, default=220): cv.positive_int,
-        cv.Optional(CONF_DEBOUNCE_US, default=350): cv.positive_int,
+        # pins/entities...
     }
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[cg.CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID])  # <-- use CONF_ID from esphome.const
 
     clock = await cg.gpio_pin_expression(config[CONF_CLOCK_PIN])
     data  = await cg.gpio_pin_expression(config[CONF_DATA_PIN])
@@ -67,14 +45,14 @@ async def to_code(config):
     bit_count   = await cg.get_variable(config[CONF_BIT_COUNT])
     reading     = await cg.get_variable(config[CONF_READING])
 
-    cg.add(var.last_card_ts   .set_parent(last_card))
-    cg.add(var.card_type_ts   .set_parent(card_type))
-    cg.add(var.card_colour_ts .set_parent(card_colour))
-    cg.add(var.bit_count_s    .set_parent(bit_count))
-    cg.add(var.reading_bs     .set_parent(reading))
+    cg.add(var.set_last_card(last_card))
+    cg.add(var.set_card_type(card_type))
+    cg.add(var.set_card_colour(card_colour))
+    cg.add(var.set_bit_count(bit_count))
+    cg.add(var.set_reading(reading))
 
-    cg.add(var.net2_bits    .set((config[CONF_NET2_BITS])))
-    cg.add(var.switch2_bits .set((config[CONF_SWITCH2_BITS])))
-    cg.add(var.debounce_us  .set((config[CONF_DEBOUNCE_US])))
+    cg.add(var.set_net2_bits(config[CONF_NET2_BITS]))
+    cg.add(var.set_switch2_bits(config[CONF_SWITCH2_BITS]))
+    cg.add(var.set_debounce_us(config[CONF_DEBOUNCE_US]))
 
     await cg.register_component(var, {})
